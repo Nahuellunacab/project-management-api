c1083602149a0807a52596e55c2a52b8abcd091d
NahuelLuna <nahuel.lunacab@gmail.com>
2026-03-10 12:47:36 -0300

    feat: agregar endpoint GET /users/me

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1.auth import router as auth_router
+from app.api.v1.users import router as users_router
 
 import app.models
 
@@ -10,6 +11,7 @@ app = FastAPI(
 )
 
 app.include_router(auth_router)
+app.include_router(users_router)
 
 
 @app.get("/")
