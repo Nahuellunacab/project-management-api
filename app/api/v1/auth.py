7c891d901fb4a790894d767099c69ccbcd3b4602
NahuelLuna <nahuel.lunacab@gmail.com>
2026-03-10 12:07:42 -0300

    chore: agregar dependencia python-multipart

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
 from app.schemas.user import UserCreate, UserResponse
 from app.services.user_service import create_user, get_user_by_email
 from app.db.session import get_db
+from fastapi.security import OAuth2PasswordRequestForm
+from app.schemas.token import Token
+from app.services.auth_service import login_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -22,3 +24,19 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
     new_user = create_user(db, user)
 
     return new_user
+
+@router.post("/login", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    access_token = login_user(db, form_data.username, form_data.password)
+
+    if not access_token:
+        raise HTTPException(
+            status_code=401,
+            detail="Credenciales incorrectas"
+        )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
